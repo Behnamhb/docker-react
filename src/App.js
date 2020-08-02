80becc16720ede9fdda7e2e6d71a578a14496334
behnam <behnam.hasanbeygi@gmail.com>
2020-08-02 16:48:14 +0430

    initial project

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          behnam is good boy love it! fuck bad vibes
         </p>
         <a
           className="App-link"
